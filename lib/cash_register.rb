bae2dc7e722d4400510c819a5604de2096d485bd
Bryce B <bryce.boddy@gmail.com>
2020-04-26 05:49:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,8 +9,8 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     self.total += price*quantity
-    quantity.times do
+    quantity.times {
       cart << title
-    end
+    }
   end
 end
